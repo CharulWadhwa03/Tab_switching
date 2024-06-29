acbcafa030182477630b910dd75d5fa9ee1f44b2
CharulWadhwa03 <99791119+CharulWadhwa03@users.noreply.github.com>
2024-06-29 08:55:56 +0530

    Add files via upload

---
+++ fetchEmployment.js
@@ -0,0 +1,30 @@
+document.querySelector('.continue-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+
+    const employmentStatus = document.querySelector('.tab button.active').innerText;
+    const industry = document.querySelector('.dropdown .dropbtn.selected-option').innerText;
+    const occupation = document.querySelector('.dropdown .dropbtn.selected-option').innerText;
+    const annualIncome = document.querySelector('.annual-income-options button.active').innerText;
+
+    const formData = {
+        employmentStatus,
+        industry,
+        occupation,
+        annualIncome
+    };
+
+    fetch('http://localhost:8000/api/application/employment/create-employment', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
